58df26b650afc06e0b1bba8e3b59e0f0704c457d
eliasdoehne <elias.doehne@hemio.de>
2018-12-08 13:52:54 +0100

    Work in progress: ViNE implementation

--- vnep_approx/vine.py
+++ vnep_approx/vine.py
@@ -1,10 +1,26 @@
 import random
 from copy import deepcopy
 
+from gurobipy import GRB, LinExpr
+
 from alib import datamodel as dm
 from alib import mip as mip
 from alib import modelcreator as mc
 from alib import solutions
+
+
+class NodeMappingMethod(object):
+    DETERMINISTIC = "deterministic"
+    RANDOMIZED = "randomized"
+
+    AVAILABLE_METHODS = [DETERMINISTIC, RANDOMIZED]
+
+
+class EdgeMappingMethod(object):
+    SPLITTABLE = "splittable"
+    SHORTEST_PATH = "shortest_path"
+
+    AVAILABLE_METHODS = [SPLITTABLE, SHORTEST_PATH]
 
 
 class WiNESingleWindow(object):
@@ -14,13 +30,19 @@ class WiNESingleWindow(object):
                  optimization_callback=mc.gurobi_callback,
                  lp_output_file=None,
                  potential_iis_filename=None,
-                 logger=None):
+                 logger=None,
+                 node_mapping_method=NodeMappingMethod.DETERMINISTIC,
+                 edge_mapping_method=EdgeMappingMethod.SHORTEST_PATH,
+                 use_load_balancing_objective=False):
         self.gurobi_settings = gurobi_settings
         self.optimization_callback = optimization_callback
         self.lp_output_file = lp_output_file
         self.potential_iis_filename = potential_iis_filename
         self.logger = logger
         self.scenario = scenario
+        self.node_mapping_method = node_mapping_method
+        self.edge_mapping_method = edge_mapping_method
+        self.use_load_balancing_objective = use_load_balancing_objective
 
     def init_modelcreator(self):
         pass
@@ -29,6 +51,9 @@ class WiNESingleWindow(object):
         # TODO: Measure runtimes
         vine_instance = ViNESingleScenario(
             scenario=self.scenario,
+            node_mapping_method=self.node_mapping_method,
+            edge_mapping_method=self.edge_mapping_method,
+            use_load_balancing_objective=self.use_load_balancing_objective,
             gurobi_settings=self.gurobi_settings,
             optimization_callback=self.optimization_callback,
             lp_output_file=self.lp_output_file,
@@ -41,6 +66,11 @@ class WiNESingleWindow(object):
             mapping = vine_instance.vine_procedure_single_request(req)
             solution.add_mapping(req, mapping)
         return solution
+
+
+class SplittableMapping(solutions.Mapping):
+    def map_edge(self, ij, edge_vars):
+        self.mapping_edges[ij] = edge_vars
 
 
 class FractionalClassicMCFModel(mip.ClassicMCFModel):
@@ -75,6 +105,19 @@ class FractionalClassicMCFModel(mip.ClassicMCFModel):
                     variable_assignment[req]["edge_vars"][ij][uv] = self.var_z[req][ij][uv].x
         return variable_assignment
 
+    def create_constraints_fix_node_mappings(self, req, node_mapping_dict):
+        """ Add constraints which enforce the node mappings specified in node_mapping_dict. """
+        for i, u_mapped in node_mapping_dict.iteritems():
+            for u in req.get_allowed_nodes(i):
+                fix_i_u_mapping_constraint = LinExpr([(1.0, self.var_y[req][i][u])])
+                name = "{req}_fix_{i}_{u}".format(req=req.name, i=i, u=u)
+
+                if u == u_mapped:
+                    self.model.addConstr(fix_i_u_mapping_constraint, GRB.EQUAL, 1.0, name=name)
+                else:
+                    self.model.addConstr(fix_i_u_mapping_constraint, GRB.EQUAL, 0.0, name=name)
+        self.model.update()
+
 
 class ViNESingleScenario(object):
     """
@@ -90,12 +133,14 @@ class ViNESingleScenario(object):
 
     def __init__(self,
                  scenario,
+                 node_mapping_method,
+                 edge_mapping_method,
+                 use_load_balancing_objective,
                  gurobi_settings=None,
                  optimization_callback=mc.gurobi_callback,
                  lp_output_file=None,
                  potential_iis_filename=None,
-                 logger=None,
-                 node_mapper=None):
+                 logger=None):
         self.gurobi_settings = gurobi_settings
         self.optimization_callback = optimization_callback
         self.lp_output_file = lp_output_file
@@ -103,53 +148,71 @@ class ViNESingleScenario(object):
         self.logger = logger
         self.scenario = scenario
 
-        self.residual_capacity_substrate = deepcopy(scenario.substrate)
-        if node_mapper is None:
-            node_mapper = DeterministicNodeMapper()
-        self.node_mapper = node_mapper
+        self.node_mapping_method = node_mapping_method
+        self.edge_mapping_method = edge_mapping_method
+        self.use_load_balancing_objective = use_load_balancing_objective
 
-        self.current_request = None
+        self.residual_capacity_substrate = deepcopy(scenario.substrate)
+        if node_mapping_method == NodeMappingMethod.DETERMINISTIC:
+            self.node_mapper = DeterministicNodeMapper()
+        elif node_mapping_method == NodeMappingMethod.RANDOMIZED:
+            self.node_mapper = RandomizedNodeMapper()
+        else:
+            raise ValueError("Invalid node mapping method: {}".format(node_mapping_method))
+
+        if self.edge_mapping_method not in EdgeMappingMethod.AVAILABLE_METHODS:
+            raise ValueError("Invalid edge mapping method: {}".format(edge_mapping_method))
+
+        self._current_request = None
+        self._provisional_node_allocations = None
+        self._provisional_edge_allocations = None
 
     def vine_procedure_single_request(self, request):
         """ Perform the ViNE procedure for a single request. """
-        self.current_request = request
-        single_req_scenario = dm.Scenario(
-            name="{}_{}".format(self.scenario.name, self.current_request.name),
-            substrate=self.residual_capacity_substrate,
-            requests=[self.current_request],
-            objective=self.scenario.objective,
-        )
+        self._current_request = request
+        self._initialize_provisional_allocations()
 
-        sub_mc = FractionalClassicMCFModel(
-            single_req_scenario, gurobi_settings=self.gurobi_settings, logger=self.logger
-        )
-        sub_mc.init_model_creator()
-        lp_variables = sub_mc.compute_fractional_solution()
-        node_variables = lp_variables[self.current_request]["node_vars"]
+        lp_variables = self.solve_vne_lp_relax()
+        if lp_variables is None:
+            return None  # REJECT: no LP solution
+        node_variables = lp_variables[self._current_request]["node_vars"]
 
-        mapping = solutions.Mapping(
-            "{}_{}".format(self.scenario.name, self.current_request.name),
-            self.current_request, self.scenario.substrate, is_embedded=True,
-        )
-        for i in self.current_request.nodes:
+        mapping = self._get_empty_mapping_of_correct_type()
+        for i in self._current_request.nodes:
             u = self.node_mapper.get_single_node_mapping(i, node_variables)
             if u is None:
-                return  # REJECT
+                return None  # REJECT: Failed node mapping
+            t = request.get_type(i)
+            self._provisional_node_allocations[(u, t)] += request.get_node_demand(i)
             mapping.map_node(i, u)
 
-        for ij in self.current_request.edges:
+        if self.edge_mapping_method == EdgeMappingMethod.SHORTEST_PATH:
+            mapping = self._map_edges_shortest_path(mapping)
+        elif self.edge_mapping_method == EdgeMappingMethod.SPLITTABLE:
+            mapping = self._map_edges_splittable(mapping)
+        else:
+            raise ValueError("Invalid edge mapping method: {}".format(self.edge_mapping_method))
+
+        if mapping is None:
+            return None  # REJECT: Failed edge mapping
+
+        self._apply_provisional_allocations_to_residual_capacity_substrate()
+        return mapping
+
+    def _map_edges_shortest_path(self, mapping):
+        for ij in self._current_request.edges:
             i, j = ij
+            ij_demand = self._current_request.get_edge_demand(ij)
             u = mapping.get_mapping_of_node(i)
             v = mapping.get_mapping_of_node(j)
-
             uv_path = self._shortest_substrate_path_respecting_capacities(
-                u, v, self.current_request.get_edge_demand(ij)
+                u, v, ij_demand
             )
             if uv_path is None:
-                return  # REJECT
+                return None
             mapping.map_edge(ij, uv_path)
-
-        self._adjust_residual_capacities(mapping)
+            for uv in uv_path:
+                self._provisional_edge_allocations[uv] += ij_demand
         return mapping
 
     def _shortest_substrate_path_respecting_capacities(self, start, target, min_capacity):
@@ -163,7 +226,8 @@ class ViNESingleScenario(object):
                 break
             q.remove(u)
             for uv in self.residual_capacity_substrate.get_out_edges(u):
-                if self.residual_capacity_substrate.get_edge_capacity(uv) < min_capacity:
+                residual_cap = self.residual_capacity_substrate.get_edge_capacity(uv) - self._provisional_edge_allocations[uv]
+                if residual_cap < min_capacity:
                     continue  # avoid using edges that are too small
                 v = next(v for v in uv if v != u)
                 new_dist = distance[u] + self.residual_capacity_substrate.get_edge_cost(uv)
@@ -179,15 +243,78 @@ class ViNESingleScenario(object):
             u = prev[u]
         return list(reversed(path))
 
-    def _adjust_residual_capacities(self, m):
-        for i, u in m.mapping_nodes.iteritems():
-            i_type = self.current_request.get_type(i)
-            i_demand = self.current_request.get_node_demand(i)
-            self.residual_capacity_substrate.node[u]["capacity"][i_type] -= i_demand
-        for ij, uv_path in m.mapping_edges.iteritems():
-            ij_demand = self.current_request.get_edge_demand(ij)
-            for uv in uv_path:
-                self.residual_capacity_substrate.edge[uv]["capacity"] -= ij_demand
+    def _map_edges_splittable(self, mapping):
+        lp_vars = self.solve_vne_lp_relax(mapping.mapping_nodes)
+        if lp_vars is None:
+            return None
+
+        lp_vars = lp_vars[self._current_request]["edge_vars"]
+
+        for ij in self._current_request.edges:
+            mapping.map_edge(ij, lp_vars[ij])
+            ij_demand = self._current_request.get_edge_demand(ij)
+            for uv, alloc in lp_vars[ij].iteritems():
+                self._provisional_edge_allocations[ij][uv] += alloc * ij_demand
+
+        return mapping
+
+    def solve_vne_lp_relax(self, fixed_node_mappings_dict=None):
+        single_req_scenario = dm.Scenario(
+            name="{}_{}".format(self.scenario.name, self._current_request.name),
+            substrate=self.residual_capacity_substrate,
+            requests=[self._current_request],
+            objective=dm.Objective.MIN_COST,
+        )
+
+        sub_mc = FractionalClassicMCFModel(
+            single_req_scenario, gurobi_settings=self.gurobi_settings, logger=self.logger
+        )
+        sub_mc.init_model_creator()
+        if fixed_node_mappings_dict is not None:
+            sub_mc.create_constraints_fix_node_mappings(self._current_request, fixed_node_mappings_dict)
+        lp_variables = sub_mc.compute_fractional_solution()
+        return lp_variables
+
+    def _initialize_provisional_allocations(self):
+        self._provisional_node_allocations = {
+            (u, t): 0.0
+            for u in self.residual_capacity_substrate.nodes
+            for t in self.residual_capacity_substrate.get_supported_node_types(u)
+        }
+        self._provisional_edge_allocations = {
+            uv: 0.0 for uv in self.residual_capacity_substrate.get_edges()
+        }
+
+    def _apply_provisional_allocations_to_residual_capacity_substrate(self):
+        """
+        Apply the current request's provisional allocations.
+
+        This should only be called once a request is certain to be accepted, as it modifies the
+        capacities available in the residual_capacity_substrate, which impact embeddings of future
+        requests.
+
+        :param m:
+        :return:
+        """
+        for node_resource, alloc in self._provisional_node_allocations.iteritems():
+            u, node_type = node_resource
+            self.residual_capacity_substrate.node[u]["capacity"][node_type] -= alloc
+        for uv, alloc in self._provisional_edge_allocations.iteritems():
+            self.residual_capacity_substrate.edge[uv]["capacity"] -= alloc
+
+    def _get_empty_mapping_of_correct_type(self):
+        if self.edge_mapping_method == EdgeMappingMethod.SHORTEST_PATH:
+            return solutions.Mapping(
+                "{}_{}".format(self.scenario.name, self._current_request.name),
+                self._current_request, self.scenario.substrate, is_embedded=True,
+            )
+        elif self.edge_mapping_method == EdgeMappingMethod.SPLITTABLE:
+            return SplittableMapping(
+                "{}_{}".format(self.scenario.name, self._current_request.name),
+                self._current_request, self.scenario.substrate, is_embedded=True,
+            )
+        else:
+            raise ValueError("Invalid edge mapping method: {}".format(self.edge_mapping_method))
 
 
 class AbstractViNENodeMapper(object):
@@ -197,6 +324,7 @@ class AbstractViNENodeMapper(object):
 
 class DeterministicNodeMapper(AbstractViNENodeMapper):
     def get_single_node_mapping(self, i, node_variables):
+        """ Deterministic node mapping: Node mapping is selected according to the maximal variable in the LP solution. """
         u_max = None
         p_max = float('-inf')
         for u, p_u in node_variables[i].iteritems():
@@ -208,6 +336,7 @@ class DeterministicNodeMapper(AbstractViNENodeMapper):
 
 class RandomizedNodeMapper(AbstractViNENodeMapper):
     def get_single_node_mapping(self, i, node_variables):
+        """ Randomized node mapping: Node mapping is selected randomly, interpreting the LP variables as probabilities. """
         u_max = None
         draw = random.random()
         # Node mapping LP variables should already be normalized to 1
