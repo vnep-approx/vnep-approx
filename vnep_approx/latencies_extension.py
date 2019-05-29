fixed big error in calculation of bounds for Hassin algorithm

vnep_approx/latencies_extension.py
@@ -29,6 +29,7 @@ class ShortestValidPathsComputerWithLatencies(object):
         self.number_of_nodes = len(self.substrate.nodes)
         self.predecessor = np.full(self.number_of_nodes, -1, dtype=np.int32)
         self.temp_distances = np.full(self.number_of_nodes, np.inf, dtype=np.float64)
+        self.temp_latencies = np.full(self.number_of_nodes, np.inf, dtype=np.float64)
 
         self.edge_set_id_to_edge_set = self.valid_mapping_restriction_computer.get_edge_set_mapping()
         self.number_of_valid_edge_sets = self.valid_mapping_restriction_computer.get_number_of_different_edge_sets()
@@ -61,22 +62,48 @@ class ShortestValidPathsComputerWithLatencies(object):
             self.number_of_nodes += 1
 
 
+    def _compute_substrate_edge_abstraction_with_integer_nodes(self, valid_edge_set):
+        out_neighbors_with_cost = []
+
+        for num_node in range(self.number_of_nodes):
+
+            snode = self.num_id_to_snode_id[num_node]
+            neighbor_and_cost_list = []
+
+            for sedge in self.substrate.out_edges[snode]:
+                if sedge in valid_edge_set:
+                    _, out_neighbor = sedge
+                    num_id_neighbor = self.snode_id_to_num_id[out_neighbor]
+                    neighbor_and_cost_list.append((num_id_neighbor, self.edge_costs[sedge]))
+
+            out_neighbors_with_cost.append(neighbor_and_cost_list)
+
+        return out_neighbors_with_cost
+
+
+
+
     def _SPPP(self, lower, upper, eps, num_source_node, num_target_node):
         S = (lower * eps) / (self.number_of_nodes + 1)
+
+        # print "\t\t\t\t\t: ", num_source_node, num_target_node, lower, upper
 
         # TODO: save max value of c_tilde -> use to shrink distances matrix (axis 1)
         c_tilde = {}
         max_c_tilde = -1
+        min_c_tilde = np.inf
         for e in self.substrate.edges:
             val = int(self.edge_costs[e] / S) + 1
             c_tilde[e] = val
+            if val < min_c_tilde:
+                min_c_tilde = val
             if val > max_c_tilde:
                 max_c_tilde = val
 
         U_tilde = int(upper / S) + self.number_of_nodes + 1
 
 
-        print "U_tilde:", U_tilde, max_c_tilde, num_source_node, num_target_node, lower, upper
+        # print "U_tilde:", U_tilde, max_c_tilde, num_source_node, num_target_node, lower, upper
         # return FAIL
 
         # unfortunately has different size for each call to SPPP - (maybe use submatrix?)
@@ -103,6 +130,7 @@ class ShortestValidPathsComputerWithLatencies(object):
                             predecessor_info[n] = (self.snode_id_to_num_id[u], self.edge_latencies[e], self.edge_costs[e])
                             self.predecessor[n] = self.snode_id_to_num_id[u]
 
+            # TODO: return for ALL NODES -> saves much computation effort!!!!!!!!!
             if distances[num_target_node][i] <= self.limit:
                 # retrace path from t to s
                 n = num_target_node
@@ -115,8 +143,11 @@ class ShortestValidPathsComputerWithLatencies(object):
                     total_costs += cost
                     path = [self.num_id_to_snode_id[n]] + path
 
-                if distances[num_target_node][i] != total_latencies:
-                    print ("Equality check failed: dist[t][i] = {}  !=  {} = total_lat".format(distances[num_target_node][i], total_latencies))
+                # if distances[num_target_node][i] != total_latencies:
+                #     print ("Equality check failed: dist[t][i] = {}  !=  {} = total_lat".format(distances[num_target_node][i], total_latencies))
+                #
+                # if lower == upper:
+                #     print "ASSERT:\t\t", lower, " = ", total_costs
 
                 return path, total_latencies, total_costs
 
@@ -133,8 +164,6 @@ class ShortestValidPathsComputerWithLatencies(object):
             else:
                 b_up = b
 
-        print "\t\t\t\t", b_low, b_up
-
         return self._SPPP(b_low, 2 * b_up, self.epsilon, num_source_node, num_target_node)
 
 
@@ -148,27 +177,35 @@ class ShortestValidPathsComputerWithLatencies(object):
         # TODO: reuse shortest paths!?
         # TODO: try to reuse this for later nodes!
 
-        queue = [(0, num_source_node)]
+        queue = [(0, 0, num_source_node)]
+        self.temp_latencies.fill(np.inf)
+        self.temp_distances.fill(np.inf)
         self.temp_distances[num_source_node] = 0
+        self.temp_latencies[num_source_node] = 0
 
         while queue:
-            costs, node = heappop(queue)
+            total_costs, total_latencies, num_current_node = heappop(queue)
 
-            if node == num_target_node:
+            if num_current_node == num_target_node:
                 break
 
-            for u, v in self.substrate.out_edges[node]:
-                lat = self.edge_costs[(u, v)]
-                if lat <= limit:  # this is where the induced subgraph G_j comes in
-                    if costs + lat < self.temp_distances[v]:
-                        self.temp_distances[v] = costs + lat
-                        heappush(queue, (costs + lat, v))
+            for sedge in self.substrate.out_edges[self.num_id_to_snode_id[num_current_node]]:
+                num_endpoint = self.snode_id_to_num_id[sedge[1]]
+                cost = self.edge_costs[sedge]
+                lat = self.edge_latencies[sedge]
+                if cost <= limit:  # this is where the induced subgraph G_j comes in
+                    if total_costs + cost < self.temp_distances[num_endpoint]:
+                        self.temp_distances[num_endpoint] = total_costs + cost
+                        self.temp_latencies[num_endpoint] = total_latencies + lat
+                        heappush(queue, (total_costs + cost, total_latencies + lat, num_endpoint))
 
-        return self.temp_distances[num_target_node]
+        print self.temp_latencies[num_target_node]
+        return self.temp_latencies[num_target_node]
+
 
 
     def _approx_latencies(self, num_source_node, num_target_node):
-        low, high = 0, len(self.edge_levels_sorted)
+        low, high = -1, len(self.edge_levels_sorted)-1
 
         while low < high-1:
             j = int((low + high) / 2)
@@ -177,7 +214,7 @@ class ShortestValidPathsComputerWithLatencies(object):
             else:
                 low = j
 
-        lower = self.edge_levels_sorted[low]
+        lower = self.edge_levels_sorted[high]
         upper = lower * self.number_of_nodes
         return self._Hassin(lower, upper, num_source_node, num_target_node)
 
@@ -188,13 +225,19 @@ class ShortestValidPathsComputerWithLatencies(object):
 
         for edge_set_index in range(self.number_of_valid_edge_sets):
 
+            self.current_out_neighbors_with_cost = self._compute_substrate_edge_abstraction_with_integer_nodes(self.edge_set_id_to_edge_set[edge_set_index])
+
             for num_source_node in range(self.number_of_nodes):
 
                 for num_target_node in range(self.number_of_nodes):
+
+                    self.predecessor.fill(-1)
+
+                    print "from ", self.num_id_to_snode_id[num_source_node], " to", self.num_id_to_snode_id[num_target_node]
+
                     path, lat, costs = self._approx_latencies(num_source_node, num_target_node)
 
-                    print "from ", self.num_id_to_snode_id[num_source_node], " to", \
-                        self.num_id_to_snode_id[num_target_node], ":\n\t", path, lat, costs
+                    print "results:\n\t", path, lat, costs
 
                     print "predecessors:\n", self.predecessor, "\n"
 
@@ -207,6 +250,7 @@ class ShortestValidPathsComputerWithLatencies(object):
                         self.edge_mapping_invalidities = True
                     elif lat > self.limit:
                         self.latency_limit_overstepped = True
+                        print "WARNING: Latency limit overstepped by ", lat - self.limit
 
                 converted_pred_dict = {self.num_id_to_snode_id[num_node]: self.num_id_to_snode_id[self.predecessor[num_node]]
                     if self.predecessor[num_node] != -1 else None for num_node in range(self.number_of_nodes)}