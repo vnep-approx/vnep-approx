b0d2d08fe2c6dd425d21f323bbd40053a3c0ca63
Balazs Nemeth <balazs.nemeth@tmit.bme.hu>
2019-06-24 19:59:49 +0200

    Add running inicialization through separation LP

--- vnep_approx/treewidth_based_fog_model.py
+++ vnep_approx/treewidth_based_fog_model.py
@@ -20,11 +20,15 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 import time
+import copy
+import math
+
 from gurobipy import GRB
 
 import treewidth_model as twm
 from alib import datamodel
 
+EPSILON = 0.00001
 
 class RandRoundSepLPOptDynVMPCollectionForFogModelResult(twm.RandRoundSepLPOptDynVMPCollectionResult):
 
@@ -56,6 +60,18 @@ class RandRoundSepLPOptDynVMPCollectionForFogModel(twm.RandRoundSepLPOptDynVMPCo
                                                                              number_further_mappings_to_add,
                                                                              gurobi_settings,
                                                                              logger)
+        # create a profit variant from the base class
+        scenario_with_unit_profit = copy.deepcopy(scenario)
+        scenario_with_unit_profit.objective = datamodel.Objective.MAX_PROFIT
+        for req in scenario_with_unit_profit.requests:
+            req.profit = 1.0
+            if len(req.types) != 1:
+                raise ValueError("Cost minimizing separation LP is not prepared for multiple NF and/or resource types")
+        # we do not need the randomized rounding in this case, so a simple separation LP is enough
+        self.profit_variant_algorithm_instance = twm.SeparationLP_OptDynVMP(scenario_with_unit_profit,
+                                                                             gurobi_settings,
+                                                                             logger.getChild("ProfitVariantForInitialization"))
+        self.profit_variant_algorithm_instance.init_model_creator()
 
     def check_supported_objective(self):
         '''Raised ValueError if a not supported objective is found in the input scenario
@@ -70,8 +86,10 @@ class RandRoundSepLPOptDynVMPCollectionForFogModel(twm.RandRoundSepLPOptDynVMPCo
 
     def create_empty_objective(self):
         # TODO (NB): according to Matthias' thesis, this should be inicialized by solving a profit version with all profits set to 1. and
-        # calculate the initial objective value based on the found initial embedding of the requests. (is there easier way?)
-        self.model.setObjective(10, GRB.MINIMIZE)
+        # calculate the initial objective value based on the found initial embedding of the requests.
+        # TODO: (is there easier way?): this initialization might be completly ignored due to the actions taken in
+        # get_first_mappings_for_requests, this builds the variables which are involved in the objective.
+        self.model.setObjective(0, GRB.MINIMIZE)
 
     def create_empty_request_embedding_bound_constraints(self):
         self.embedding_bound = {req : None for req in self.requests}
@@ -96,6 +114,58 @@ class RandRoundSepLPOptDynVMPCollectionForFogModel(twm.RandRoundSepLPOptDynVMPCo
             dynvmp_instance.reinitialize(new_node_costs=self.dual_costs_node_resources,
                                          new_edge_costs=self.dual_costs_edge_resources)
             self.dynvmp_runtimes_initialization[req].append(time.time() - single_dynvmp_reinit_time)
+
+    def get_first_mappings_for_requests(self):
+        """
+        For the cost variant the first request mappings come from solving a profit variant with universal 1.0 cost for each request.
+        Secondarily this is a feasibility test: if the achieved profit equals the number of requests, then this scenario is feasible.
+
+        :return:
+        """
+        self.profit_variant_algorithm_instance.compute_solution()
+        # TODO: this feasibility checking is needed! now we can ommit it until, we have generated proper minimization scenarios
+        # if not self.profit_variant_algorithm_instance.status.hasFeasibleStatus() or\
+        #   math.fabs(self.profit_variant_algorithm_instance.status.getObjectiveValue() - len(self.scenario.requests)) > EPSILON:
+        #     raise ValueError("Scenario is unfeasible, not all requests can be mapped at the same time!")
+        # Add all mapping variables for each req's each valid mapping found by the initial optimization
+        universal_node_type = self.substrate.types.copy().pop()
+        for req, req_profit_variant in zip(self.requests, self.profit_variant_algorithm_instance.requests):
+            for index, gurobi_var  in enumerate(self.profit_variant_algorithm_instance.mapping_variables[req_profit_variant]):
+                valid_mappings_of_req = self.profit_variant_algorithm_instance.mappings_of_requests[req_profit_variant]
+                allocations_of_sinlge_valid_mapping = self._compute_allocations(req, valid_mappings_of_req[index])
+                # save allocations for usage at the constraints (so we wont have to model.update() after each variable addition)
+                self.allocations_of_mappings[req].append(allocations_of_sinlge_valid_mapping)
+                sum_cost_of_valid_mapping = 0.0
+                for sres, alloc in allocations_of_sinlge_valid_mapping.iteritems():
+                    res_cost = None
+                    if type(sres) is tuple and universal_node_type not in sres:
+                        res_cost = self.substrate.edge[sres]['cost']
+                    elif type(sres) is str:
+                        res_cost = self.substrate.node[sres]['cost'][universal_node_type]
+                    if res_cost is not None:
+                        sum_cost_of_valid_mapping += res_cost * alloc
+                # keep the variable name convention used before
+                new_var = self.model.addVar(lb=0.0, ub=1.0,
+                                              obj=sum_cost_of_valid_mapping,
+                                              vtype=GRB.CONTINUOUS, name=gurobi_var.VarName)
+                self.mapping_variables[req].append(new_var)
+                # mappings can be safely reused, just redirect the references to the current algorithm's object
+                for mapping in valid_mappings_of_req:
+                    mapping.request = req
+                    mapping.substrate = self.substrate
+                    self.mappings_of_requests[req] = mapping
+        # make added variables avaialbe
+        self.model.update()
+        for req in self.requests:
+            for index, gurobi_var in enumerate(self.mapping_variables[req]):
+                for sres, gurobi_constr in self.capacity_constraints.iteritems():
+                    allocations_of_sinlge_valid_mapping = self.allocations_of_mappings[req][index]
+                    # if this valid mapping has no allocations for the 'sres' substrate resource, we can skip it
+                    if sres not in allocations_of_sinlge_valid_mapping:
+                        continue
+                    self.model.chgCoeff(gurobi_constr, gurobi_var, allocations_of_sinlge_valid_mapping[sres])
+        # apply coefficient changes
+        self.model.update()
 
     def perform_separation_and_introduce_new_columns(self, current_objective, ignore_requests=[]):
         new_columns_generated = False
